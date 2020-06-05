71382c7fa9a4f76203adb832e5aa1c537d714e1f
jeromrichard <jeromerichard72@yahoo.com.com>
2020-06-05 11:09:41 +0200

    test ajout 1 collab

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,12 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome Olivier,in this area test.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/Menu/">Go to menu</Link> <br />
+    <Link to="/Menu/">Voir les collaborateurs</Link> <br />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
