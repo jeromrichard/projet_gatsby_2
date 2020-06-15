d9f0babdb4e4bff3d5ff790966302d0573194937
jeromrichard <jeromerichard72@yahoo.com.com>
2020-06-15 17:36:03 +0200

    lien page accueil sur menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import { Link } from "gatsby";
 
 const Menu = () => {
   const data = useStaticQuery(graphql`
@@ -36,7 +37,7 @@ const Menu = () => {
         ))}
       </ul>
     </div>
-    
+    <Link to="/">Go back to the homepage</Link>
   );
 };
 
